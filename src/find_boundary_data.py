1bbf616292ae6e051876c18c765f18a3345a762c
wellido <707376149@qq.com>
2019-04-03 15:52:06 +0900

    edit fbd code

--- src/find_boundary_data.py
+++ src/find_boundary_data.py
@@ -84,7 +84,7 @@ def search_in_order(model, select_first, select_second, coefficient, iterations)
     for i in range(iterations):
         coefficient = coefficient + bias
         # print("coefficient: ", coefficient)
-        synthetic_data = np.clip(coefficient * select_first + (1 - coefficient) * select_second, 0.0, 1.0)
+        synthetic_data = np.clip(coefficient * select_first + (1 - coefficient) * select_second, -1.0, 1.0)
         y_synthetic = model.predict(synthetic_data).argmax(axis=-1)[0]
         # print("prediction: ", y_synthetic)
         if y_synthetic == y_first and y_previous == y_second:
@@ -98,8 +98,8 @@ def search_in_order(model, select_first, select_second, coefficient, iterations)
         print("save coefficient: ", save_coefficient)
         back_coefficient = coefficient - bias
         front_coefficient = coefficient + bias
-        back_data = np.clip(back_coefficient * select_first + (1 - back_coefficient) * select_second, 0.0, 1.0)
-        front_data = np.clip(front_coefficient * select_first + (1 - front_coefficient) * select_second, 0.0, 1.0)
+        back_data = np.clip(back_coefficient * select_first + (1 - back_coefficient) * select_second, -1.0, 1.0)
+        front_data = np.clip(front_coefficient * select_first + (1 - front_coefficient) * select_second, -1.0, 1.0)
         print("front prediction: ", model.predict(front_data).argmax(axis=-1)[0])
         print("back prediction: ", model.predict(back_data).argmax(axis=-1)[0])
         return front_data, back_data
@@ -108,63 +108,130 @@ def search_in_order(model, select_first, select_second, coefficient, iterations)
         return False
 
 
-def search_by_dichotomize(model, select_first, select_second, coefficient, coefficient_plus, final_plus, iterations):
+def new_search_in_order(model_path, first_label_path, second_label_path, coefficient, generate_num, iterations, save_path):
     """
-    search data using dichotomize
-    :param model:
-    :param select_first:
-    :param select_second:
+
+    :param model_path:
+    :param first_label_path:
+    :param second_label_path:
+    :param coefficient:
+    :param generate_num:
+    :param iterations:
+    :param save_path:
+    :return:
+    """
+    model = load_model(model_path)
+    data_first_list = np.load(first_label_path)["x_train"]
+    data_second_list = np.load(second_label_path)["x_train"]
+    data_first_list = data_first_list / 127.5 - 1.
+    data_second_list = data_second_list / 127.5 - 1.
+    front_side_save_list = []
+    back_side_save_list = []
+    count = 0
+    while count < generate_num:
+        select_first = data_first_list[random.randint(0, len(data_first_list) - 1)]
+        select_second = data_second_list[random.randint(0, len(data_second_list) - 1)]
+        select_first = select_first / 127.5 - 1.
+        select_second = select_second / 127.5 - 1.
+        select_first = select_first.reshape(1, 28, 28, 1)
+        select_second = select_second.reshape(1, 28, 28, 1)
+        bias = 1 / iterations
+
+        for i in range(iterations):
+            coefficient = coefficient + bias
+            # print("coefficient: ", coefficient)
+            synthetic_data = np.clip(coefficient * select_first + (1 - coefficient) * select_second, -1.0, 1.0)
+            y_synthetic = model.predict(synthetic_data).argmax(axis=-1)[0]
+
+
+
+    ...
+
+
+def search_by_dichotomize(model_path, first_label_path, second_label_path, generate_num, this_coefficient, this_coefficient_plus, final_plus, iterations, save_path):
+    """
+
+    :param model_path:
+    :param first_label_path:
+    :param second_label_path:
+    :param generate_num:
     :param coefficient:
     :param coefficient_plus:
     :param final_plus:
     :param iterations:
+    :param save_path:
     :return:
     """
+    model = load_model(model_path)
+    data_first_list = np.load(first_label_path)["x_train"]
+    data_second_list = np.load(second_label_path)["x_train"]
+    print(data_second_list.shape)
 
-    final_plus = final_plus
-    y_first = model.predict(select_first).argmax(axis=-1)[0]
-    y_second = model.predict(select_second).argmax(axis=-1)[0]
-    stop_flag = 0
-    previous_prediction = y_first
-    success_flag = 1
-    for i in range(iterations):
-        synthetic_data = np.clip(coefficient * select_first + (1 - coefficient) * select_second, 0.0, 1.0)
-        prediction = model.predict(synthetic_data).argmax(axis=-1)[0]
-        # print("times: ", i)
-        # print("coefficient: ", coefficient)
-        # print("prediction: ", prediction)
-        coefficient_plus = coefficient_plus / 2
-        if prediction == y_first:
-            if previous_prediction == y_second:
-                stop_flag = 0
+    # save data
+    front_side_save_list = []
+    back_side_save_list = []
+    count = 0
+    while count < generate_num:
+        print("generate data number: ", count)
+        coefficient = this_coefficient
+        coefficient_plus = this_coefficient_plus
+        select_first = data_first_list[random.randint(0, len(data_first_list) - 1)]
+        select_second = data_second_list[random.randint(0, len(data_second_list) - 1)]
+        select_first = select_first / 127.5 - 1.
+        select_second = select_second / 127.5 - 1.
+        select_first = select_first.reshape(1, 28, 28, 1)
+        select_second = select_second.reshape(1, 28, 28, 1)
+        final_plus = final_plus
+        y_first = model.predict(select_first).argmax(axis=-1)[0]
+        y_second = model.predict(select_second).argmax(axis=-1)[0]
+        stop_flag = 0
+        previous_prediction = y_first
+        success_flag = 1
+
+        for i in range(iterations):
+            synthetic_data = np.clip(coefficient * select_first + (1 - coefficient) * select_second, -1.0, 1.0)
+            prediction = model.predict(synthetic_data).argmax(axis=-1)[0]
+            # print("times: ", i)
+            # print("coefficient: ", coefficient)
+            # print("prediction: ", prediction)
+            coefficient_plus = coefficient_plus / 2
+            if prediction == y_first:
+                data_first_list = np.append(data_first_list, ((synthetic_data + 1.) * 127.5).astype(int).reshape(1, 28, 28), axis=0)
+                if previous_prediction == y_second:
+                    stop_flag = 0
+                else:
+                    stop_flag += 1
+                coefficient = coefficient - coefficient_plus
+                previous_prediction = y_first
+            elif prediction == y_second:
+                data_second_list = np.append(data_second_list, ((synthetic_data + 1.) * 127.5).astype(int).reshape(1, 28, 28), axis=0)
+                if previous_prediction == y_first:
+                    stop_flag = 0
+                else:
+                    stop_flag += 1
+                coefficient = coefficient + coefficient_plus
+                previous_prediction = y_second
             else:
-                stop_flag += 1
-            coefficient = coefficient - coefficient_plus
-            previous_prediction = y_first
-        elif prediction == y_second:
-            if previous_prediction == y_first:
-                stop_flag = 0
-            else:
-                stop_flag += 1
-            coefficient = coefficient + coefficient_plus
-            previous_prediction = y_second
+                success_flag = 0
+                break
+            if stop_flag > 5:
+                break
+        if success_flag == 1:
+            back_coefficient = coefficient - final_plus
+            front_coefficient = coefficient + final_plus
+            print("back: ", back_coefficient)
+            print("front: ", front_coefficient)
+            back_data = np.clip(back_coefficient * select_first + (1 - back_coefficient) * select_second, -1.0, 1.0)
+            front_data = np.clip(front_coefficient * select_first + (1 - front_coefficient) * select_second, -1.0, 1.0)
+            print("front prediction: ", model.predict(front_data).argmax(axis=-1)[0])
+            print("back prediction: ", model.predict(back_data).argmax(axis=-1)[0])
+            front_side_save_list.append(front_data)
+            back_side_save_list.append(back_data)
+            count += 1
         else:
-            success_flag = 0
-            break
-        if stop_flag > 5:
-            break
-    if success_flag == 1:
-        back_coefficient = coefficient - final_plus
-        front_coefficient = coefficient + final_plus
-        back_data = np.clip(back_coefficient * select_first + (1 - back_coefficient) * select_second, 0.0, 1.0)
-        front_data = np.clip(front_coefficient * select_first + (1 - front_coefficient) * select_second, 0.0, 1.0)
-        print("front prediction: ", model.predict(front_data).argmax(axis=-1)[0])
-        print("back prediction: ", model.predict(back_data).argmax(axis=-1)[0])
-        print(back_data)
-        return front_data, back_data
-    else:
-        print("can't find data.")
-        return False
+            print("can't find data.")
+    np.savez(save_path, x_train=[front_side_save_list, back_side_save_list])
+
 
 
 def search_by_switch_pixel(model, select_first, select_second, switch_num):
@@ -241,7 +308,7 @@ def search_all_data(model_path, first_label_path, second_label_path, coefficient
         select_second = select_second / 127.5 - 1.
         select_first = select_first.reshape(1, 28, 28, 1)
         select_second = select_second.reshape(1, 28, 28, 1)
-        synthetic_data = search_in_order(model, select_first, select_second, coefficient, 5000)
+        synthetic_data = search_in_order(model, select_first, select_second, coefficient, 3000)
         # synthetic_data = search_by_switch_pixel(model, select_first, select_second, 1)
         if synthetic_data:
             front_data = synthetic_data[0]
@@ -286,11 +353,45 @@ def get_arg_and_run():
     search_all_data(model_path, first_label_path, second_label_path, coefficient, generate_num, save_path)
 
 
+def get_arg_and_run_dichotomize():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str,
+                        help="dnn model path")
+    parser.add_argument("--first_label_path", type=str,
+                        help="one side boundary label data path")
+    parser.add_argument("--second_label_path", type=str,
+                        help="the other side boundary label data path")
+    parser.add_argument("--coefficient", type=float, default=0.00,
+                        help="coefficient * data1 + (1 - coefficient) * data2")
+    parser.add_argument("--coefficient_plus", type=float, default=0.00,
+                        help="...")
+    parser.add_argument("--final_plus", type=float, default=0.00,
+                        help="...")
+    parser.add_argument("--generate_num", type=int,
+                        help="generate data number")
+    parser.add_argument("--iterations", type=int,
+                        help="max try times")
+    parser.add_argument("--save_path", type=str,
+                        help="data save path")
+    args = parser.parse_args()
+    model_path = args.model_path
+    first_label_path = args.first_label_path
+    second_label_path = args.second_label_path
+    coefficient = args.coefficient
+    generate_num = args.generate_num
+    save_path = args.save_path
+    coefficient_plus = args.coefficient_plus
+    final_plus = args.final_plus
+    iterations = args.iterations
+    search_by_dichotomize(model_path, first_label_path, second_label_path, generate_num, coefficient, coefficient_plus,
+                          final_plus, iterations, save_path)
+
+
 if __name__ == '__main__':
-    coefficient = 0.00
-    coefficient_plus = 0.50
-    model_path = "../model/lenet-5.h5"
-    data_dir_path = "../data/original_data/"
+    # coefficient = 0.00
+    # coefficient_plus = 0.50
+    # model_path = "../model/lenet-5.h5"
+    # data_dir_path = "../data/original_data/"
     # model = load_model("../model/lenet-5.h5")
     # data_first_list = np.load("../data/original_data/class_0.npz")["x_train"]
     # data_second_list = np.load("../data/original_data/class_1.npz")["x_train"]
@@ -308,7 +409,10 @@ if __name__ == '__main__':
     # search_all_data("../model/lenet-5.h5", "../data/original_data/class_0.npz", "../data/original_data/class_1.npz",
     #                 0.00, 1000, "../data/boundary_data/data_0&1.npz")
 
-    get_arg_and_run()
+    # get_arg_and_run()
+    get_arg_and_run_dichotomize()
+
+    # python ../src/find_boundary_data.py --model_path ../model/lenet5_label01_newprepocess.h5 --first_label_path ../data/original_data/class_0.npz --second_label_path ../data/original_data/class_1.npz --coefficient 0.5 --coefficient_plus 0.5 --final_plus 0.00005 --generate_num 20000 --iterations 500 --save_path "../data/2_label_model/data_0&1/dichotomize_01_20000.npz"
 
     # python find_boundary_data.py --model_path ../model/lenet-5.h5 --first_label_path ../data/original_data/class_0.npz --second_label_path ../data/original_data/class_1.npz --coefficient 0.00 --generate_num 10 --save_path ../data/boundary_data/data_0&1.npz
 
